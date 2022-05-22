6f2d3cb3fe6cd082c81e53c9cf0bfda19b925452
NehaBhageriya <nehabhageriya99@gmail.com>
2022-05-23 01:55:52 +0530

    3rd react assignment complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style.css';
 
 function App() {
+  const [data, setData] = useState({
+    userName: "",
+    dept: "",
+    rate: "",
+  })
+  const [collect, collectData] = useState([])
+  const [next, setNext] = useState(false);
+  // const { userName, dept, rate } = data
+  const userHandler = (u) => {
+    const name = u.target.name;
+    const value = u.target.value;
+    console.log(name, value);
+    setData({ ...data, [name]: value })
+    // setData({ ...data, [user.target.name]: [user.target.value] })
+  }
+  const submitHandler = (us) => {
+    us.preventDefault()
+    // console.log(data);
+    const newRecord = { ...data, id: new Date().getTime().toString() }
+    console.log(newRecord)
+    collectData([...collect, newRecord])
+    console.log(collect)
+    setData({ userName: " ", dept: " ", rate: " " })
+    setNext(true);
+  }
+  const back = () => {
+    setNext(false);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {next ? null :
+        <form action="" onSubmit={submitHandler}>
+<br/><br/>
+
+          <h1>EMPLOYEE FEEDBACK FORM</h1>
+          <br /><br />
+          <label>Name : </label>
+          <input type="text" name="userName" value={data.userName} onChange={userHandler} /><br /><br />
+          <label>Department : </label>
+          <input type="text" name="dept" value={data.dept} onChange={userHandler} /><br /><br />
+          <label>Rating : </label>
+          <input type="text" name="rate" value={data.rate} onChange={userHandler} /><br /><br /><br />
+          <input className="submit" type="submit" />
+        </form>}
+      {next ?
+        <div>
+<br/><br/>
+
+          <h1>EMPLOYEE FEEDBACK DATA</h1>
+<br/><br/>
+          <div className="box">
+
+            {
+              collect.map((ele) => {
+                let { id, userName, dept, rate } = ele
+                return (
+                  <div className="subbox" key={id}>
+                    Name:{userName}| Department :{dept} | Rating :{rate}
+                  </div>
+                )
+              })
+            }
+          </div>
+<br/><br/><br/>
+{next?<input onClick={back} className="submit" type="button" value="Go Back"/>:null}
+        </div>:null}
     </div>
   );
 }
